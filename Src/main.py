3871bec081d60b83801bc343329c1bc9e4d72461
Aayush <just.another.3d.object@gmail.com>
2023-08-09 23:23:16 +0530

    Setting up the game window & running the mainloop

--- Src/main.py
+++ Src/main.py
@@ -6,10 +6,21 @@ from const import *
 class Main:
 
     def __init__(self):
-        pass
+        # Initializing the pygame model
+        pygame.init() 
+
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption("Chess")
 
     def mainloop(self):
-        pass
+        # Setting up the pygame file
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            
+            pygame.display.update()
 
 main = Main()
 main.mainloop()
