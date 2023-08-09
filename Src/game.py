313bba9f4ce51f116b6cebe69b866c3ef3a2c1bb
Aayush <just.another.3d.object@gmail.com>
2023-08-09 23:33:19 +0530

    Drawing the chess board

--- Src/game.py
+++ Src/game.py
@@ -1,3 +1,8 @@
+import itertools
+import pygame
+
+from const import *
+
 class Game:
 
     def __init__(self):
@@ -6,4 +11,8 @@ class Game:
     # Show methods
     
     def show_bg(self, surface):
-        pass
+        for row, col in itertools.product(range(ROWS), range(COLS)):
++            color = (234, 235, 200) if (row+col)%2 == 0 else (119, 154, 88)
++            rect = (col * SQSIZE, row * SQSIZE, SQSIZE, SQSIZE)
+ 
++            pygame.draw.rect(surface, color, rect)
